22b6c921fd1d34d140f69c8bb0bbf6506b6e7b10
Safkat khan Tafsir <safkatkhan420@gmail.com>
2024-01-20 17:50:12 +0600

    Revision

---
+++ All/revission.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+/*
+	var i int
+	fmt.Print("Enter any integer number: ")
+	fmt.Scanln(&i)
+	fmt.Println("Your Entered Number is ", i)
+*/
+	var s string
+	fmt.Print("Enter Your Name : ")
+	fmt.Scanln(&s)
+	fmt.Println("Hello",s)
+
+}
